f777f699c3cf70ee5f9565819a4c43f8e1ba17fe
shotasuzuki <shotasuzuki01@gmail.com>
2024-02-26 22:06:42 +0900

    3-10提出

---
+++ MiddleSkill/3-12-5/src/main/java/com/example/demo/dto/UserRequest.java
@@ -0,0 +1,44 @@
+package com.example.demo.dto;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+import lombok.Data;
+
+@Data
+public class UserRequest implements Serializable {
+
+	/**
+	 * 名前
+	 */
+	@NotEmpty(message = "名前を入力してください")
+	@Size(max = 100, message = "名前は100桁以内で入力してください")
+	private String name;
+
+	/**
+	 * 年齢
+	 */
+	@NotEmpty(message = "年齢を入力してください")
+	@Size(max = 999, message = "年齢は３桁以内で入力してください")
+	private String age;
+
+	/**
+	 * 性別
+	 */
+	private int Gender_id;
+
+	/**
+	 * 登録日
+	 */
+
+	private Date createDate;
+
+	/**
+	 * 血液型
+	 */
+	private int blood_type_id;
+
+}
